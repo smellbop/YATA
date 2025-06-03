27f242e26929080f3ecf810dfcacce7309437135
Elliot Pollard <sm08esp@gmail.com>
2025-06-03 16:33:48 +0100

    Initial Commit

---
+++ YATA/YATAApp.swift
@@ -0,0 +1,17 @@
+//
+//  YATAApp.swift
+//  YATA
+//
+//  Created by Elliot Pollard on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct YATAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
